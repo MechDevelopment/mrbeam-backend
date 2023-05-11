b482de0f6f22d4c0f6a728f49081f86eb8cf0ae4
Victor <55928886+vpvpvpvp@users.noreply.github.com>
2023-05-11 16:53:51 +0300

    Integrate ml service

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,23 +1,15 @@
+use actix_multipart::form::MultipartForm;
+use actix_web::{web, App, Error, HttpResponse, HttpServer, Responder};
 use image::EncodableLayout;
 use reqwest::{multipart, Client};
 
-use actix_multipart::form::MultipartForm;
-use actix_web::{web, App, Error, HttpResponse, HttpServer, Responder};
-
-#[derive(serde::Deserialize, serde::Serialize)]
-struct Beam {
-    xmin: f32,
-    ymin: f32,
-    xmax: f32,
-    ymax: f32,
-    confidence: f32,
-    name: String,
-}
+use api::services::MLService;
+use api::models::Beam;
 
 #[derive(serde::Serialize)]
 struct PredictionInfo {
     data: Vec<Beam>,
-    uuid: String
+    uuid: String,
 }
 
 #[derive(Debug, MultipartForm)]
@@ -31,43 +23,27 @@ async fn health() -> impl Responder {
 
 async fn predict(
     MultipartForm(form): MultipartForm<UploadForm>,
-    client: web::Data<Client>,
+    ml_client: web::Data<MLService>,
 ) -> Result<impl Responder, Error> {
     let bytes = form.file.data.as_bytes().to_vec();
 
-    let hash = sha256::digest_bytes(&bytes);
+    let beams = ml_client.predict(bytes).await.unwrap();
 
-    let beam_file = multipart::Part::bytes(bytes)
-        .file_name("beam.png")
-        .mime_str("image/png")
-        .unwrap();
-    
-    let form = multipart::Form::new().part("file", beam_file);
-
-    let res = client
-        .post("https://vktrpnzrv.fvds.ru/predict")
-        .multipart(form)
-        .send()
-        .await
-        .unwrap();
-    
-    let beams: Vec<Beam> = res.json().await.unwrap();
-
-    Ok(HttpResponse::Ok().json(
-        PredictionInfo {
-            data: beams,
-            uuid: String::from("ue12-aks8-ca82- 892l"),
-        }
-    ))
+    Ok(HttpResponse::Ok().json(PredictionInfo {
+        data: beams,
+        uuid: String::from("ue12-aks8-ca82- 892l"),
+    }))
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let client = web::Data::new(Client::new());
+    let ml_client = web::Data::new(MLService::new(String::from(
+        "https://vktrpnzrv.fvds.ru",
+    )));
 
     HttpServer::new(move || {
         App::new()
-            .app_data(client.clone())
+            .app_data(ml_client.clone())
             .route("/health", web::get().to(health))
             .route("/predict", web::post().to(predict))
     })
