9fba76080229c524c8dfbc50f158cb8e00f6c487
Victor <55928886+vpvpvpvp@users.noreply.github.com>
2023-06-09 19:46:34 +0300

    Add db migration

---
+++ api/migrations/20230511173616_create_prediction_table.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE predictions(
+    id UUID PRIMARY KEY NOT NULL DEFAULT (uuid_generate_v4()),
+    created_at TIMESTAMP
+    WITH
+        TIME ZONE DEFAULT NOW(),
+    prediction JSONB,
+    correction JSONB
+);
