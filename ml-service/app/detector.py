f0d2dee9754316f7d3cf031f65073f2afc1c631a
Victor <55928886+vpvpvpvp@users.noreply.github.com>
2023-06-12 23:22:47 +0300

    Add preprocess class for yolo

--- ml-service/app/detector.py
+++ ml-service/app/detector.py
@@ -1,10 +1,10 @@
 import abc
+from typing import Tuple, List
 import torch
 import numpy as np
 
-import logging
-
-logger = logging.getLogger(__name__)
+from utils.augmentations import letterbox
+from utils.general import non_max_suppression
 
 
 class Yolo5Detector:
@@ -21,3 +21,23 @@ class Yolo5Detector:
         detections[["xmin", "xmax"]] = detections[["xmin", "xmax"]] / image.shape[1]
         detections[["ymin", "ymax"]] = detections[["ymin", "ymax"]] / image.shape[0]
         return detections.to_dict(orient="records")
+
+
+class YOLOPreProcess:
+    def __init__(self, image_size: Tuple[int, int], stride: int = 32, auto: bool = False):
+        self._image_size = image_size
+        self._stride = stride
+        self._auto = auto
+
+    def process(self, image: np.ndarray) -> Tuple[np.ndarray, List[int]]:
+        im = letterbox(image, self._image_size, stride=self._stride, auto=self._auto)[0]
+        im = im.transpose((2, 0, 1))
+        im = np.ascontiguousarray(im)
+
+        im = torch.from_numpy(im)
+        im = im.float()
+        im /= 255
+
+        im = im.unsqueeze(0)
+
+        return im.numpy(), im.shape
