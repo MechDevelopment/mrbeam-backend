c445847e808aa472a4f82dfc70ef2dae0b3ef079
Victor <55928886+vpvpvpvp@users.noreply.github.com>
2023-06-26 13:12:01 +0300

    Update default settings

--- ml-service/app/settings.py
+++ ml-service/app/settings.py
@@ -8,11 +8,11 @@ class Settings(BaseSettings):
     app_dir: str = os.path.abspath(os.path.dirname(__file__))
     project_root: str = os.path.abspath(os.path.join(app_dir, os.pardir))
     port: int = 8011
-    environment: str = "development"
+    environment: str = "dev"
 
-    model_weights: str = os.path.join(project_root, "weights/yolo5l_640.pt")
+    model_weights: str = os.path.join(project_root, "weights/best_s_june.onnx")
     model_conf: float = 0.4
-    model_image_size: int = 640
+    image_size: int = 640
 
     class Config:
         env_file = ".env"
