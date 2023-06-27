d2dea96146f1615fb94ac11e59e0cccc9ae78cef
Victor <55928886+vpvpvpvp@users.noreply.github.com>
2023-06-27 13:01:35 +0300

    Remove torchub version of the detector

--- ml-service/app/detector.py
+++ ml-service/app/detector.py
@@ -12,22 +12,6 @@ from app.utils.augmentations import letterbox
 from app.utils.general import non_max_suppression, scale_coords
 
 
-class Yolo5Detector:
-    def __init__(self, weights_path: str, conf: float = 0.08):
-        self.model = torch.hub.load(
-            "ultralytics/yolov5",
-            "custom",
-            path=weights_path,
-        )
-        self.model.conf = conf
-
-    def __call__(self, image: np.ndarray):
-        detections = self.model([image], size=640).pandas().xyxy[0]
-        detections[["xmin", "xmax"]] = detections[["xmin", "xmax"]] / image.shape[1]
-        detections[["ymin", "ymax"]] = detections[["ymin", "ymax"]] / image.shape[0]
-        return detections.to_dict(orient="records")
-
-
 class YOLOPreProcess:
     def __init__(
         self, image_size: Tuple[int, int], stride: int = 32, auto: bool = False
