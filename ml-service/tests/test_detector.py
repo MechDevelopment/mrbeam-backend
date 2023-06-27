24fe0642a9283a8abba0c7ecb1b68df45c7695a2
Victor <55928886+vpvpvpvp@users.noreply.github.com>
2023-06-27 19:41:58 +0300

    Add test for postprocessor

--- ml-service/tests/test_detector.py
+++ ml-service/tests/test_detector.py
@@ -2,6 +2,7 @@ import pytest
 
 import numpy as np
 import cv2
+import torch
 
 from app.settings import Settings
 from app import detector
@@ -11,7 +12,7 @@ from app import detector
 def settings():
     settings = Settings()
     settings.image_size = 640
-    
+
     return settings
 
 
@@ -26,3 +27,20 @@ def test_given_image_is_then_preprocessed_correctly_for_yolo(settings, image):
     
     assert isinstance(prep, np.ndarray)
     assert prep.shape == (1, 3, 640, 640)
+
+
+def test_given_valid_inputs_then_yolo_returns_valid_outputs(settings, image):
+    ...
+
+
+def test_given_valid_raw_outputs_are_then_correctly_postprocessed(image, settings):
+    outputs = torch.load("tests/resources/raw_yolo_outputs.pt")
+    postprocessor = detector.YOLOPostProcess()
+
+    preds = postprocessor.process(
+                outputs, image.shape[:2], (1, 3, settings.image_size, settings.image_size)
+            )
+    
+    assert len(preds) > 0
+    for key in ["xmin", "ymin", "xmax", "ymax", "conf", "class"]:
+        assert key in preds[0]
