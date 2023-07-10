174d4497ada7f5a2fb52ab928cc95f7a9c8859db
Victor <55928886+vpvpvpvp@users.noreply.github.com>
2023-07-10 16:57:06 +0300

    Change beam model

--- api/src/models.rs
+++ api/src/models.rs
@@ -4,8 +4,8 @@ pub struct Beam {
     pub ymin: f32,
     pub xmax: f32,
     pub ymax: f32,
-    pub confidence: Option<f32>,
-    pub name: String,
+    pub conf: Option<f32>,
+    pub class: f32,
 }
 
 pub struct PredictionId {
